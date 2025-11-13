4ba038281dc085846e90a62f44176c7e79c231b8
VasanthiKuppuswamy <Vasanthik@gmx.net>
2025-11-13 08:19:27 +0100

    Added ASCII stages

--- snowman.py
+++ snowman.py
@@ -1,7 +1,35 @@
+
 import random
 
-# List of secret words
-WORDS = ["python", "git", "github", "snowman", "meltdown"]
+STAGES = [
+     # Stage 0: Full snowman
+     """
+      ___  
+     /___\\ 
+     (o o) 
+     ( : ) 
+     ( : ) 
+     """,
+     # Stage 1: Bottom part starts melting
+     """
+      ___  
+     /___\\ 
+     (o o) 
+     ( : ) 
+     """,
+     # Stage 2: Only the head remains
+     """
+      ___  
+     /___\\ 
+     (o o) 
+     """,
+     # Stage 3: Snowman completely melted
+     """
+      ___  
+     /___\\ 
+     """
+ ]
+WORDS = ["python", "winterblues",  "git", "github", "snowman", "meltdown"]
 
 
 def get_random_word():
