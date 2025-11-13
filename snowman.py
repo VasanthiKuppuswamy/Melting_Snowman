1d6cdd822a9b4b106c80981d99e7e10971211dea
VasanthiKuppuswamy <Vasanthik@gmx.net>
2025-11-13 08:48:58 +0100

    added get_valid_guess function and game logic

--- snowman.py
+++ snowman.py
@@ -55,7 +55,20 @@ def print_game_header():
     print("⭐" * 25)
     print("\nGuess the word before your snowman melts!\n")
 
+def get_valid_guess(guessed_letters):
+    """Validate and return a single unused letter from user input.
+    Return validated lower case letter."""
+    while True:
+        guess = input("Guess a letter: ").lower()
 
+        if len(guess) != 1:
+            print("Please enter only one character.")
+        elif not guess.isalpha():
+            print("Please enter a letter (a-z).")
+        elif guess in guessed_letters:
+            print("You have already guessed this letter. Try again.")
+        else:
+            return guess
 def play_game():
     """The main function using the gameplay loop"""
     secret_word = get_random_word()
@@ -64,14 +77,35 @@ def play_game():
     mistakes = 0
     max_mistakes = len(STAGES) - 1
 
-
-
     while mistakes < max_mistakes:
         display_game_state(mistakes, secret_word, guessed_letters)
-        break
-    # For now, simply prompt the user once:
-    guess = input("Guess a letter: ").lower()
-    print("You guessed:", guess)
+
+        # Check if player has won
+        if all(letter in guessed_letters for letter in secret_word):
+            print("\n CONGRATULATIONS! YOU SAVED THE SNOWMAN! ")
+            print(f"The word was: {secret_word.upper()}")
+            print("⭐" * 25)
+            return
+
+        guess = get_valid_guess(guessed_letters)
+        guessed_letters.append(guess)
+
+        if guess not in secret_word:
+            mistakes += 1
+            print("Incorrect! Snowman melts...")
+        else:
+            print("Correct!")
+
+        # This executes if while loop completes normally (player lost)
+    display_game_state(mistakes, secret_word, guessed_letters)
+    print("Oh no! The snowman melted completely!")
+    print(f"The word was: {secret_word}")
+
 
 if __name__ == "__main__":
-    play_game()
+    while True:
+        play_game()
+        user_input = input(
+            "Would you like to end the game? Please press 'n'. If you would like to continue, press any other key.: ")
+        if user_input.lower() == "n":
+            break
