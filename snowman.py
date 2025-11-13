13418a00340ae1f464d1679b58c5bc1866566c98
VasanthiKuppuswamy <Vasanthik@gmx.net>
2025-11-13 08:25:54 +0100

    Added display_game_state function

--- snowman.py
+++ snowman.py
@@ -36,13 +36,36 @@ def get_random_word():
     """Selects a random word from the list."""
     return WORDS[random.randint(0, len(WORDS) - 1)]
 
+def display_game_state(mistakes, secret_word, guessed_letters):
+    """Display the snowman stage for the current number of mistakes."""
+    print(STAGES[mistakes])
+    # Build a display version of the secret word.
+    display_word = ""
+    for letter in secret_word:
+        if letter in guessed_letters:
+            display_word += letter + " "
+        else:
+            display_word += "_ "
+    print("Word: ", display_word, "\n")
+
+
 
 def play_game():
+    """The main function using the gameplay loop"""
     secret_word = get_random_word()
     print("Welcome to Snowman Meltdown!")
     print("Secret word selected: " + secret_word)  # for testing, later remove this line
 
 
+    guessed_letters = []
+    mistakes = 0
+    max_mistakes = len(STAGES) - 1
+
+
+
+    while mistakes < max_mistakes:
+        display_game_state(mistakes, secret_word, guessed_letters)
+        break
     # For now, simply prompt the user once:
     guess = input("Guess a letter: ").lower()
     print("You guessed:", guess)
